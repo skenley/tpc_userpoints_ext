c5e54b7c511487885136fc31b5300338eac53351
AntonioXIII <aj72325@gmail.com>
2020-01-01 15:09:58 -0700

    QuizFirstPass Event Subscriber
    Created an event subscriber to listen for when a user first passes a
    quiz. The subscriber awards the user the amount of points assigned to
    quiz via the QuizConfig entity stored for the quiz node, if it exists.

---
+++ src/EventSubscriber/QuestionnaireEventSubscriber.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Drupal\tpc_userpoints_ext\EventSubscriber;
+
+use Drupal\questionnaire\Event\QuizFirstPassEvent;
+use Drupal\tpc_userpoints_ext\Entity\QuizConfig;
+use Drupal\tpc_userpoints_ext\UserPointsTransactionWrapper;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class QuestionnaireEventSubscriber implements EventSubscriberInterface {
+  
+  public static function getSubscribedEvents() {
+    
+    return [
+      QuizFirstPassEvent::EVENT_NAME => 'quizFirstPassed',
+    ];
+    
+  }
+  
+  public function quizFirstPassed(QuizFirstPassEvent $event) {
+    
+    $quiz = $event->getQuiz();
+    $user = $event->getUser();
+    
+    // Load the QuizConfig for this quiz node. If it exists, apply a 
+    // userpoints transaction for passing the quiz. Otherwise, do
+    // nothing.
+    $conf = QuizConfig::load($quiz->id());
+    
+    if(!empty($conf)) {
+      
+      $newTran = new UserPointsTransactionWrapper('userpoints_default_points', 
+        'userpoints_q_quiz_passed', $user, strval($conf->getPointValue()));
+      $newTran->execute();
+      
+    }
+    
+  }
+  
+}
